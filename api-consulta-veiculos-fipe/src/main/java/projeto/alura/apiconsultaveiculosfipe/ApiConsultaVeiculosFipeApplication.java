988b89eb60a0b072484be98db5af12436992457f
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2024-04-26 11:31:12 -0300

    commit start initial

---
+++ api-consulta-veiculos-fipe/src/main/java/projeto/alura/apiconsultaveiculosfipe/ApiConsultaVeiculosFipeApplication.java
@@ -0,0 +1,13 @@
+package projeto.alura.apiconsultaveiculosfipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultaVeiculosFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultaVeiculosFipeApplication.class, args);
+	}
+
+}
---
+++ api-consulta-veiculos-fipe/src/test/java/projeto/alura/apiconsultaveiculosfipe/ApiConsultaVeiculosFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.alura.apiconsultaveiculosfipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultaVeiculosFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
